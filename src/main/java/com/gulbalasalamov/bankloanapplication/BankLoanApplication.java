eefe69c5d0a3d77ab96c18a262127d05c745c71c
Gulbala Salamov <g.salamov@gmail.com>
2022-08-09 15:44:16 +0300

    project init with README

---
+++ src/main/java/com/gulbalasalamov/bankloanapplication/BankLoanApplication.java
@@ -0,0 +1,13 @@
+package com.gulbalasalamov.bankloanapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankLoanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankLoanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gulbalasalamov/bankloanapplication/BankLoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gulbalasalamov.bankloanapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankLoanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
