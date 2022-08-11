24b3ff523de34c4e5f5f7fff126cacb3322591c9
Gulbala Salamov <g.salamov@gmail.com>
2022-08-11 09:21:20 +0300

    feat: CustomerDTO added

---
+++ src/main/java/com/gulbalasalamov/bankloanapplication/model/dto/CustomerDTO.java
@@ -0,0 +1,29 @@
+package com.gulbalasalamov.bankloanapplication.model.dto;
+
+import javax.validation.constraints.*;
+
+public class CustomerDTO {
+    @Pattern(regexp = "[1-9][0-9]{10}")
+    private String nationalIdentityNumber;
+
+    @NotBlank(message = "name can not be null")
+    private String name;
+
+    @NotBlank(message = "surname can not be null")
+    private String surname;
+
+    @NotBlank(message = "phone can not be null")
+    private String phone;
+
+    @Email
+    private String email;
+
+    @NotNull(message = "monthly income can not be null")
+    @Min(1)
+    private Double monthlyIncome;
+
+    private String gender;
+
+    @Min(18)
+    private Integer age;
+}
