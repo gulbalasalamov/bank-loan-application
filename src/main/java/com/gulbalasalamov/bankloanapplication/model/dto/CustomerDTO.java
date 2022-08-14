9712f35432999a81c710381c17d1c3d927b5aa28
Gulbala Salamov <g.salamov@gmail.com>
2022-08-14 12:56:32 +0300

    chore: clean up

--- src/main/java/com/gulbalasalamov/bankloanapplication/model/dto/CustomerDTO.java
+++ src/main/java/com/gulbalasalamov/bankloanapplication/model/dto/CustomerDTO.java
@@ -13,30 +13,28 @@ import java.util.List;
 @NoArgsConstructor
 public class CustomerDTO {
 
-   // private Long id;
-
-   // @Pattern(regexp = "[1-9][0-9]{10}")
+    @Pattern(regexp = "[1-9][0-9]{10}")
     private String nationalIdentityNumber;
 
-   // @NotBlank(message = "name can not be null")
+    @NotBlank(message = "name can not be null")
     private String firstName;
 
-    //@NotBlank(message = "surname can not be null")
+    @NotBlank(message = "surname can not be null")
     private String lastName;
 
-    //@NotBlank(message = "phone can not be null")
+    @NotBlank(message = "phone can not be null")
     private String phone;
 
-//    @Email
+    @Email
     private String email;
 
-    //@NotNull(message = "monthly income can not be null")
-//    @Min(1)
+    @NotNull(message = "monthly income can not be null")
+    @Min(1)
     private Double monthlyIncome;
 
     private String gender;
 
-//    @Min(18)
+    @Min(18)
     private Integer age;
 
     private Integer loanScore;
